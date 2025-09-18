751e068f905275f37269ef42b096806b35b6f954
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-09-18 10:07:21 -0300

    feat: adicionar banco.sql

---
+++ banco.sql
@@ -0,0 +1,33 @@
+-- Criação do banco de dados
+CREATE DATABASE IF NOT EXISTS trabalhoa3;
+USE trabalhoa3;
+
+-- Criação da tabela categoria
+CREATE TABLE IF NOT EXISTS categoria (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    tamanho ENUM('PEQUENO', 'MEDIO', 'GRANDE') NOT NULL,
+    embalagem ENUM('LATA', 'VIDRO', 'PLASTICO') NOT NULL
+);
+
+-- Criação da tabela produto
+CREATE TABLE IF NOT EXISTS produto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    preco_unitario DECIMAL(10,2) NOT NULL,
+    unidade VARCHAR(20) NOT NULL,
+    quantidade_estoque INT NOT NULL,
+    quantidade_minima INT NOT NULL,
+    quantidade_maxima INT NOT NULL,
+    categoria VARCHAR(100) NOT NULL
+);
+
+-- Criação da tabela registro
+CREATE TABLE IF NOT EXISTS registro (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data DATE NOT NULL,
+    tipo VARCHAR(100) NOT NULL,
+    quantidade INT NOT NULL,
+    movimentacao VARCHAR(20) NOT NULL,
+    status VARCHAR(20) NOT NULL
+);
